5c8b388196e0d045feff0bbafd0ca0956dfbb518
Николай Романенко <bsbsov2@gmail.com>
2025-09-13 14:17:26 +0300

    last3

--- database.sql
+++ database.sql
@@ -1,8 +1,8 @@
 CREATE TABLE Programs (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    title VARCHAR UNIQUE NOT NULL,
+    name VARCHAR UNIQUE NOT NULL,
     price NUMERIC NOT NULL,
-    type VARCHAR NOT NULL,
+    program_type VARCHAR NOT NULL,
     created_at TIMESTAMP DEFAULT NOW() NOT NULL,
     updated_at TIMESTAMP DEFAULT NOW()
 );
@@ -10,34 +10,35 @@ CREATE TABLE Programs (
 CREATE TABLE Modules (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     program_id BIGINT REFERENCES Programs(id) NOT NULL,
-    title VARCHAR NOT NULL,
+    name VARCHAR NOT NULL,
     description TEXT,
     created_at TIMESTAMP DEFAULT NOW() NOT NULL,
     updated_at TIMESTAMP DEFAULT NOW(),
-    UNIQUE(program_id, title)
+    deleted_at TIMESTAMP,
+    UNIQUE(program_id, name)
 );
 
 CREATE TABLE Courses (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     module_id BIGINT REFERENCES Modules(id) NOT NULL,
-    title VARCHAR NOT NULL,
+    name VARCHAR NOT NULL,
     description TEXT,
     created_at TIMESTAMP DEFAULT NOW() NOT NULL,
     updated_at TIMESTAMP DEFAULT NOW(),
-    UNIQUE(module_id, title)
+    UNIQUE(module_id, name)
 );
 CREATE TABLE Lessons (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     course_id BIGINT REFERENCES Courses(id) NOT NULL,
-    title VARCHAR NOT NULL,
-    body TEXT NOT NULL,
-    url_video TEXT,
+    name VARCHAR NOT NULL,
+    content TEXT NOT NULL,
+    video_url TEXT,
     position INT NOT NULL,
     created_at TIMESTAMP DEFAULT NOW() NOT NULL,
     updated_at TIMESTAMP DEFAULT NOW(),
-    UNIQUE(course_id, position)
+    UNIQUE(course_id, name)
 );
-CREATE TABLE TeachingGroups (
+CREATE TABLE Teaching_Groups (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     slug TEXT UNIQUE NOT NULL,
     created_at TIMESTAMP DEFAULT NOW() NOT NULL,
@@ -46,13 +47,14 @@ CREATE TABLE TeachingGroups (
 CREATE TYPE users_type AS ENUM ('student', 'teacher', 'admin');
 CREATE TABLE Users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    group_id BIGINT REFERENCES TeachingGroups(id) NOT NULL,
-    type users_type NOT NULL,
+    teaching_group_id BIGINT REFERENCES Teaching_Groups(id) NOT NULL,
+    role users_type NOT NULL,
     name VARCHAR NOT NULL,
     email VARCHAR NOT NULL UNIQUE,
-    password VARCHAR NOT NULL,
+    password_hash VARCHAR NOT NULL,
     created_at TIMESTAMP DEFAULT NOW() NOT NULL,
-    updated_at TIMESTAMP DEFAULT NOW()
+    updated_at TIMESTAMP DEFAULT NOW(),
+    deleted_at TIMESTAMP
 );
 CREATE TYPE enrollments_type AS ENUM ('active', 'pending', 'cancelled', 'completed');
 CREATE TABLE Enrollments (
@@ -67,21 +69,21 @@ CREATE TABLE Enrollments (
 CREATE TYPE payments_type AS ENUM ('pending', 'paid', 'failed', 'refunded');
 CREATE TABLE Payments (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    enrollment_id BIGINT REFERENCES Enrollments(id) NOT NULL,
+    id_enrollment BIGINT REFERENCES Enrollments(id) NOT NULL,
     amount NUMERIC NOT NULL,
     status payments_type NOT NULL,
-    date TIMESTAMP DEFAULT NOW() NOT NULL,
+    paid_at TIMESTAMP DEFAULT NOW() NOT NULL,
     created_at TIMESTAMP DEFAULT NOW() NOT NULL,
     updated_at TIMESTAMP DEFAULT NOW()
 );
 CREATE TYPE programcompletion_type AS ENUM ('active', 'completed', 'pending', 'cancelled');
-CREATE TABLE ProgramCompletions (
+CREATE TABLE Program_Completions (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     user_id BIGINT REFERENCES Users(id) NOT NULL,
     program_id BIGINT REFERENCES Programs(id) NOT NULL,
     status programcompletion_type NOT NULL,
-    start_time TIMESTAMP DEFAULT NOW() NOT NULL,
-    finish_time TIMESTAMP DEFAULT NOW(),
+    started_at TIMESTAMP DEFAULT NOW() NOT NULL,
+    completed_at TIMESTAMP DEFAULT NOW(),
     created_at TIMESTAMP DEFAULT NOW() NOT NULL,
     updated_at TIMESTAMP DEFAULT NOW()
 );
@@ -89,31 +91,32 @@ CREATE TABLE Certificates (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     user_id BIGINT REFERENCES Users(id) NOT NULL,
     program_id BIGINT REFERENCES Programs(id) NOT NULL,
-    certificate_url VARCHAR NOT NULL,
-    certificate_created_at TIMESTAMP DEFAULT NOW() NOT NULL,
+    url VARCHAR NOT NULL,
+    issued_at TIMESTAMP DEFAULT NOW() NOT NULL,
     created_at TIMESTAMP DEFAULT NOW() NOT NULL,
     updated_at TIMESTAMP DEFAULT NOW()
 );
 CREATE TABLE Quizzes (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     lesson_id BIGINT REFERENCES Lessons(id) NOT NUll,
-    title VARCHAR NOT NULL,
-    body TEXT NOT NULL,
+    name VARCHAR NOT NULL,
+    content TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT NOW() NOT NULL,
     updated_at TIMESTAMP DEFAULT NOW()
 );
 CREATE TABLE Exercises (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     lesson_id BIGINT REFERENCES Lessons(id) NOT NULL,
-    title VARCHAR NOT NULL,
-    url_pr VARCHAR NOT NULL,
+    name VARCHAR NOT NULL,
+    url VARCHAR NOT NULL,
     created_at TIMESTAMP DEFAULT NOW() NOT NULL,
     updated_at TIMESTAMP DEFAULT NOW()
 );
 CREATE TABLE Discussions (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     lesson_id BIGINT REFERENCES Lessons(id) NOT NULL,
-    body TEXT NOT NULL,
+    user_id BIGINT REFERENCES Users(id) NOT NULL,
+    text TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT NOW() NOT NULL,
     updated_at TIMESTAMP DEFAULT NOW()
 );
@@ -121,8 +124,8 @@ CREATE TYPE blog_type AS ENUM ('created', 'in moderation', 'published', 'archive
 CREATE TABLE Blog (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     user_id BIGINT REFERENCES Users(id) NOT NULL,
-    title VARCHAR NOT NULL,
-    body TEXT NOT NULL,
+    name VARCHAR NOT NULL,
+    content TEXT NOT NULL,
     status blog_type NOT NULL,
     created_at TIMESTAMP DEFAULT NOW() NOT NULL,
     updated_at TIMESTAMP DEFAULT NOW()
